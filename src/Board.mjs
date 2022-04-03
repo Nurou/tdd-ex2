ecd8c473edd96293120834734ceb49ea7b22487d
Joel Hassan <joel.nhn@gmail.com>
2022-04-03 12:32:04 +0300

    The board starts empty

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,12 +2,30 @@ export class Board {
   width;
   height;
 
+  grid;
+
   constructor(width, height) {
     this.width = width;
     this.height = height;
+
+    const grid = [];
+
+    // generate grid
+    for (let row = 0; row < this.height; row++) {
+      let currentRow = [];
+      for (let col = 0; col < this.width; col++) {
+        if (col === this.width - 1) {
+          currentRow.push(".\n");
+        } else {
+          currentRow.push(".");
+        }
+      }
+      grid.push(currentRow);
+    }
+    this.grid = grid;
   }
 
   toString() {
-    return "TODO";
+    return this.grid.flat().join("");
   }
 }

9dff0c4f0eb515b6621290805cfc6e9813c3c4f2
Joel Hassan <joel.nhn@gmail.com>
2022-04-24 00:42:55 +0300

    refactor tetromino class

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -2,54 +2,30 @@ import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
   static T_SHAPE(initialOrientation = 0) {
-    return new Tetromino(
-      `TTT
-    .T.`,
-      4,
-      initialOrientation,
-      [
-        new RotatingShape("....\nTTT.\n.T.."),
-        new RotatingShape(".T..\nTT..\n.T.."),
-        new RotatingShape("....\n.T..\nTTT."),
-        new RotatingShape(".T..\n.TT.\n.T.."),
-      ]
-    );
+    return new Tetromino(initialOrientation, [
+      new RotatingShape("....\nTTT.\n.T.."),
+      new RotatingShape(".T..\nTT..\n.T.."),
+      new RotatingShape("....\n.T..\nTTT."),
+      new RotatingShape(".T..\n.TT.\n.T.."),
+    ]);
   }
 
   static I_SHAPE(initialOrientation = 0) {
-    return new Tetromino(
-      `IIII
-    ....
-    ....`,
-      2,
-      initialOrientation,
-      [
-        new RotatingShape("....\nIIII\n....\n...."),
-        new RotatingShape("..I.\n..I.\n..I.\n..I."),
-      ]
-    );
+    return new Tetromino(initialOrientation, [
+      new RotatingShape("....\nIIII\n....\n...."),
+      new RotatingShape("..I.\n..I.\n..I.\n..I."),
+    ]);
   }
   static O_SHAPE(initialOrientation = 0) {
-    return new Tetromino(
-      `.OO
-      .OO
-      ...`,
-      1,
-      initialOrientation,
-      [new RotatingShape(".OO.\n.OO.\n....")]
-    );
+    return new Tetromino(initialOrientation, [
+      new RotatingShape(".OO.\n.OO.\n...."),
+    ]);
   }
 
-  shape;
   orientations = [];
   currentOrientationIndex = 0;
-  maxOrientations;
 
-  constructor(shape, maxOrientations, currentOrientationIndex, orientations) {
-    this.shape = shape;
-
-    this.maxOrientations = maxOrientations;
-
+  constructor(currentOrientationIndex, orientations) {
     let index;
 
     if (currentOrientationIndex > orientations.length - 1) {
@@ -62,35 +38,15 @@ export class Tetromino {
 
     this.currentOrientationIndex = index;
 
-    // TODO: remove once shape orientations are hardcoded
-    if (orientations.length === 0) {
-      let orientation = new RotatingShape(shape);
-
-      for (let i = 0; i < maxOrientations; i++) {
-        this.orientations.push(orientation);
-        orientation = orientation.rotateRight();
-      }
-    } else {
-      this.orientations = orientations;
-    }
+    this.orientations = orientations;
   }
 
   rotateRight() {
-    return new Tetromino(
-      this.shape,
-      this.maxOrientations,
-      this.currentOrientationIndex + 1,
-      this.orientations
-    );
+    return new Tetromino(this.currentOrientationIndex + 1, this.orientations);
   }
 
   rotateLeft() {
-    return new Tetromino(
-      this.shape,
-      this.maxOrientations,
-      this.currentOrientationIndex - 1,
-      this.orientations
-    );
+    return new Tetromino(this.currentOrientationIndex - 1, this.orientations);
   }
 
   toString() {
